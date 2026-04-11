8bd1c994f35a5a4ca360df76e1e3d2ca1899feb7
GabrielFaria1509 <fariagabriel1509@gmail.com>
2026-04-11 19:01:16 -0300

    Update TesteApostilaErasure.m

--- Códigos/TesteApostilaErasure.m
+++ Códigos/TesteApostilaErasure.m
@@ -1,44 +1,35 @@
-% =========================================================================
-% Script de Teste - Erasure Decoding (Baseado na Apostila de Sarah Johnson)
-% =========================================================================
 clc; clear;
 
-% 1. Matriz de Paridade H (Exemplo 1.12)
-H = [1 1 0 1 0 0;
-     0 1 1 0 1 0;
-     1 0 0 0 1 1;
-     0 0 1 1 0 1];
+% 1. Gera a matriz H com o Base Graph
+B = [0 2 -1 1; 
+     1 -1 2 0];
+Zc = 3;
+s = 5;
+H = BaseGraphLifting(B, Zc, s);
+%Descubro o núemro de colunas de m da matriz H gerada
+m = width(H);
 
-max_iter = 10; % Limite de segurança
+% 2. Gera a mensagem aleatória (sabemos que para esse B e Zc, são 12 bits)
+y_original = geradormessage(m);
 
-%% Teste 1: O Cenário de Sucesso (Exemplo 2.2)
-disp('======================================================');
-disp(' TESTE 1: Exemplo 2.2 - Apagamentos Recuperáveis');
-disp('======================================================');
-% Vetor recebido: [0 0 1 x x x] (usamos -1 para o 'x')
-y1 = [0, 0, 1, -1, -1, -1]; 
+% 3. Insere os apagamentos (usando -1)
+num_apagamentos = 3;
 
+posicoes_apagadas = randperm(width(y_original),num_apagamentos)
+
+disp('Vetor Recebido com Apagamentos (-1):');
+y1 = y_original;
+y1(posicoes_apagadas) = -1;
+
+disp('Mensagem Original:');
+disp(y_original);
+
+disp('Vetor Recebido com Apagamentos (-1):');
+disp(y1);
+
+% 4. Roda o decodificador
+max_iter = 30;
 [M1, iter1] = erasureDecoding(y1, H, max_iter);
 
-fprintf('Iterações necessárias: %d\n', iter1);
 disp('Vetor Decodificado pelo MATLAB:');
 disp(M1);
-disp('Vetor Esperado pela Apostila:');
-disp('     0     0     1     0     1     1');
-disp(' ');
-
-
-%% Teste 2: O Cenário de Falha (Exemplo 4.3)
-disp('======================================================');
-disp(' TESTE 2: Exemplo 4.3 - Falha por Stopping Set');
-disp('======================================================');
-% Vetor recebido: [0 0 x 0 x x] (usamos -1 para o 'x')
-y2 = [0, 0, -1, 0, -1, -1];
-
-[M2, iter2] = erasureDecoding(y2, H, max_iter);
-
-fprintf('Iterações rodadas: %d (Atingiu o limite max_iter)\n', iter2);
-disp('Vetor Resultante pelo MATLAB (Note que os -1 continuam):');
-disp(M2);
-disp('A apostila confirma que o decodificador falha pois {3,5,6} é um Stopping Set.');
-disp('======================================================');
