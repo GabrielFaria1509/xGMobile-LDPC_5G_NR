3c6583aba7e548a7af9c7c7236df8697633e7187
GabrielFaria1509 <fariagabriel1509@gmail.com>
2026-04-16 15:50:58 -0300

    Enhance Bit-Flipping test with noise simulation
    Added noise simulation to the Bit-Flipping test script.

--- Códigos/Códigos Gabriel/teste_BitFlipping.m
+++ Códigos/Códigos Gabriel/teste_BitFlipping.m
@@ -25,11 +25,36 @@ m = width(H);
 % Vetor aleatório recebido
 y = geradormessage(m);
 
+%Exibir mensagem antes de sofrer ruído
+disp("Mensagem original sem ruído")
+display(y);
+
+%Simulação de canal com ruído
+% Adicionando ruído ao vetor recebido
+noise_level = 0.5; 
+
+%Crio um vetor de 1 linha e colunas m,gera aleatórios entre 0 e 1(rand)
+% Onde for menor que 0.1, ele marca como 1 (erro), caso contrário 0 (ok).
+mascara_erros = rand(1,m) < noise_level
+
+disp("Máscara de erros");
+disp(double(mascara_erros));
+
+
+y_ruidoso = double(xor(y,mascara_erros));
+
+% Exibir o vetor ruidoso
+disp('Vetor Ruidoso:');
+disp(y_ruidoso);
+
+
 % Chamando a função que criamos
-[M, iter] = BitFlipping(y, H, max_iter);
+[M, iter] = BitFlipping(y_ruidoso, H, max_iter);
 
 fprintf('Iterações necessárias para convergência: %d\n', iter);
 disp('Vetor Recebido com Erro:');
-disp(y);
+disp(y_ruidoso);
 disp('Vetor Decodificado pelo MATLAB:');
 disp(M);
+
+
