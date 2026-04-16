3d0bb9749a4c2140fdd1ea74d851ff056c08826b
GabrielFaria1509 <fariagabriel1509@gmail.com>
2026-04-16 15:50:14 -0300

    Enhance Sum-Product Decoding with noise simulation
    Added noise simulation to the input LLR vector and updated the decoding function to return success status.

--- Códigos/Códigos Gabriel/testeSumProductDecoding.m
+++ Códigos/Códigos Gabriel/testeSumProductDecoding.m
@@ -16,6 +16,12 @@ display(H);
 % 2. Vetor LLR de entrada calculado
 r = (4*rand(1,m) - 2);
 
+%3Simular canal com ruídos
+noise_level = 0.6;
+% Gerar ruído gaussiano e adicionar ao vetor LLR de entrada
+noise = noise_level * randn(1, m);
+r = r + noise;
+
 
 % Limite de iterações definido na apostila
 max_iter = 10;
@@ -25,7 +31,7 @@ disp(' TESTE - Sum-Product Decoding');
 disp('======================================================');
 
 % Chamando a função que criamos
-[z, iter, L] = sumProductDecoding(r, H, max_iter);
+[z, iter, L,success] = sumProductDecoding(r, H, max_iter);
 
 % =========================================================================
 % Resultados para Comparação (Baseado nas páginas 35 e 36)
@@ -38,3 +44,8 @@ disp(L);
 disp('Vetor Decodificado pelo MATLAB (Hard Decision) - z:');
 disp(z);
 
+if success
+    disp("Sucesso palavra-código válida");
+else
+    disp("Falha palavra-código inválida");
+end
