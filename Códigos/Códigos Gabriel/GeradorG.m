3e96c29431ae1c83d709019a3beb63a699c26928
Gabriel Faria <fariagabriel1509@gmail.com>
2026-05-09 15:42:17 -0300

    Aviso para gerar matriz G(Zc maiores vai demorar mais)

--- Códigos/Códigos Gabriel/GeradorG.m
+++ Códigos/Códigos Gabriel/GeradorG.m
@@ -10,6 +10,7 @@ k = n - m;
 
 %Divisão da matriz H recebida em dois blocos(Matrizes)
 % Dividir a matriz H em dois blocos(A == bits de informação,B = matriz quadrada,bits de paridade)
+display("Gerando G,pode demorar apra Zc maiores")
 A = H(:,1:k);
 B = H(:,k+1:n);
 
@@ -21,7 +22,7 @@ P = gf(B)\gf(A);  %esse comanda com barra equivale a fazer inversa de B vezes A
 %Voltando P para double(Precisa tirar gf para juntar com I)
 P = double(P.x);
 
-I = speye(k); %matriz identidade do tamanho da mensagem
+I = speye(k); %matriz identidade esparsa do tamanho da mensagem
 G = [I,P']; % Combina a matriz identidade com a matriz de paridade
 %apostrófe indica transposta,vírgula junta matriz lado a lado
 
