867179b1a4d8c84245617f46028974e83f2f00b3
Gabriel Faria <fariagabriel1509@gmail.com>
2026-05-12 14:30:50 -0300

    Codificação mensagem com inserção dos filler bits

--- Códigos/Códigos Gabriel/GeradorPalavraCodigo.m
+++ Códigos/Códigos Gabriel/GeradorPalavraCodigo.m
@@ -1,14 +1,35 @@
-function [palavra_codigo] = GeradorPalavraCodigo(mensagem,G)
-%verificar se o tamanho da mensagem bate com a matriz G
-[k_G,~] = size(G);
+function [palavra_codigo, F] = GeradorPalavraCodigo(mensagem, G)
+    % Verificar se o tamanho da mensagem bate com a matriz G
+    % Extraio capacidade de informação da matriz G
+    [k_G, ~] = size(G);
+    
+    F = 0; % Quantidade de filler bits
+    B = length(mensagem); % Pacote de dados
+    
+    if B ~= k_G
+        disp("Tamanho da mensagem menor que a Matriz G");
+        disp("Iniciando implementação de filler bits...");
+        
+        F = k_G - B;
+        
+        if F < 0
+            error("O pacote é maior que a matriz. Aumente o Zc.");
+        end
+        
+        filler_bits = -1*ones(1, F); % Vetor para concatenar na mensagem
+        % Marcados como -1(equivalente NULL) temporariamente para permitir a multiplicação com G
+        mensagem = [mensagem, filler_bits];
+        
+        fprintf("Filler bits adicionados: %d\n", F);
+    end
 
-if length(mensagem) ~= k_G
-    error('Erro: O tamanho da mensagem não bate com a Matriz Geradora!');
-end
+    mensagem_camuflada = mensagem;
 
-% 2. Codificação (Multiplicação em GF(2))
-% Multiplica a mensagem original por G e aplica o módulo 2
+    %%crio uma máscara,onde for -1 troco para 0 para poder ter a
+    %%multiplcação(u*G)
+    mensagem_camuflada(mensagem == -1) = 0;
+    
+    palavra_codigo = mod(mensagem_camuflada* G, 2);
 
-palavra_codigo = mod(mensagem*G,2);
-
+    palavra_codigo(1:k_G) = mensagem;
 end
--- Códigos/Códigos Gabriel/geradormessage.m
+++ Códigos/Códigos Gabriel/geradormessage.m
@@ -1,12 +1,5 @@
-function msg = geradormessage(G)
-    % Gera um vetor linha de tamanho 'Bits' contendo 0s e 1s aleatórios
-    % 1. Extrai a quantidade de linhas da matriz G (que equivale a 'k' bits de informação)
-    % Usamos o '~' para ignorar o número de colunas, pois não precisamos dele aqui
-    [k, ~] = size(G);
-    
-    % 2. Gera um vetor linha de tamanho 'k' contendo 0s e 1s aleatórios
-    msg = randi([0 1], 1, k);
-    
+%% Função para Gerar a Mensagem Original do Usuário
+function msg_original = geradormessage(tamanho_pacote)
+    % Gera o pacote de dados real (ex: dados do WhatsApp) com 'tamanho_pacote' bits
+    msg_original = randi([0 1], 1, tamanho_pacote);
 end
-
-
