cd29e778409bd5efa62a8633c908c3f5628c7000
Jose Eduardo Pinto Teixeira <zepteixeira06@gmail.com>
2026-05-19 17:09:23 -0300

    adicionando o filler bits geral

---
+++ Códigos/CódigosGera/FillerBits.m
@@ -0,0 +1,30 @@
+function [u, F] = FillerBits(mensagem, G)
+    % Extraio capacidade de informação da matriz G
+    k_G = height(G);
+    
+    B = length(mensagem); % Pacote de dados
+    
+    if B ~= k_G
+        F = k_G - B;
+        
+        if F < 0
+            error("O pacote é maior que a matriz. Aumente o Zc.");
+        end
+        
+        filler_bits = -1*ones(1, F); % Vetor para concatenar na mensagem
+        % Marcados como -1(equivalente NULL) temporariamente para permitir a multiplicação com G
+        mensagem = [mensagem, filler_bits];
+        
+    end
+
+    mensagem_camuflada = mensagem;
+
+    %%crio uma máscara,onde for -1 troco para 0 para poder ter a
+    %%multiplcação(u*G)
+    mensagem_camuflada(mensagem == -1) = 0;
+    
+    %u é o nome dado para o array que contém a mensagem e os filler bits
+    u = mod(mensagem_camuflada* G, 2);
+
+    u(1:k_G) = mensagem;
+end
