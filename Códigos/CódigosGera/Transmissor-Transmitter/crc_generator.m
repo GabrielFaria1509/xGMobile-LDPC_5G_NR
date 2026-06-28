a8d32f32bf3627e994dadf915c4aaee829acdd77
Gabriel Faria <fariagabriel1509@gmail.com>
2026-06-28 14:11:21 -0300

    Ajuste função CRC,pronta para receber blocos da mensagem quando segmentada

--- Códigos/CódigosGera/Transmissor-Transmitter/crc_generator.m
+++ Códigos/CódigosGera/Transmissor-Transmitter/crc_generator.m
@@ -1,16 +1,30 @@
-function CRC = crc_generator(message)
-    
-    A = length(message);
+function CRC = crc_generator(message,crc_option)
 
-    if A > 3824
-        L = 24;
-        g = [1 1 0 0 0 0 1 1 0 0 1 0 0 1 1 0 0 1 1 1 1 1 0 1 1];
+     A = length(message);
 
-    else
-        L = 16;
-        g = [1 0 0 0 1 0 0 0 0 0 0 1 0 0 0 0 1];
+
+    if nargin < 2
+        if A > 3824
+            crc_option = "24A";
+        else
+            crc_option = "16";
+        end
     end
 
+   switch crc_option
+       case "24A"  %For big messages but not with code block segmentation
+            L = 24;
+            g = [1 1 0 0 0 0 1 1 0 0 1 0 0 1 1 0 0 1 1 1 1 1 0 1 1];
+       case "24B"  %%For code blocks
+            L = 24;
+            g = [1 1 0 0 0 0 0 0 0 0 0 0 0 0 0 0 0 1 1 0 0 0 0 1 1];
+       case "16" %for short messages
+            L = 16;
+            g = [1 0 0 0 1 0 0 0 0 0 0 1 0 0 0 0 1];
+        otherwise
+            error('Tipo de CRC inválido. Use: "24A", "24B" ou "16".');
+    end
+    
     temp_message = [message,zeros(1,L)];
 
     for i = 1 : A
